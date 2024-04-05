a78712fa9ccf6d11152106b20eb0d3ce61c6f6ed
Tashel Reddy <tashelreddy@yahoo.com>
2024-04-05 09:30:17 -0400

    Update fetchFlightData.js remove api key for safety

--- API/fetchFlightData.js
+++ API/fetchFlightData.js
@@ -5,7 +5,7 @@ async function fetchData() {
     const options = {
         method: 'GET',
         headers: {
-            'X-RapidAPI-Key': '46fbb75d1bmsh6956c8bb4825f1dp166e33jsn352edf6c3b5a',
+            'X-RapidAPI-Key': '',
             'X-RapidAPI-Host': 'flight-radar1.p.rapidapi.com'
         }
     };
